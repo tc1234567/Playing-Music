c2ba970a9cc3ed4043338a4da44bd960b2bae911
tc1234567 <86094343+tc1234567@users.noreply.github.com>
2021-07-17 15:12:21 -0700

    Add files via upload

--- mp3_rough.py
+++ mp3_rough.py
@@ -1,124 +1,145 @@
+#Add song button takes in user input for which song to add to the database from the file directory
+#it takes in the input and adds to the db which displays its contents in the playlist
+#Delete song deletes the selected song in the playlist
+#Play song button plays the selected song in the playlist
+#Pause song button pauses the selected song in the playlist
+
+
+#use __init__ to initialise sqlite connection
+
+import os
 from tkinter import *
 from PIL import ImageTk, Image
+from tkinter import filedialog
 import sqlite3
+import pygame
 
 #1st window - main window
 root = Tk()
-root.title('tkbutton')
-root.geometry('600x600')
+root.title('Music player')
+root.geometry('500x500')
+
+pygame.init()
+pygame.mixer.init()# initialise pygame
 
 #establishing connection to database
-conn = sqlite3.connect('button.db')
+conn = sqlite3.connect('mp3db.db')
 c = conn.cursor()
 
-#textboxes
-song1 = Entry(root, width = 30)
-song1.grid(row = 1, column = 1, padx = 20)
-singer1 = Entry(root, width = 30)
-singer1.grid(row = 2, column = 1, pady = 20)
+# c.execute("""CREATE TABLE IF NOT EXISTS mp3table (playlist text)""");
 
-#labels for the textboxes and their dimensions
-song1_label = Label(root, text = "Song")
-song1_label.grid(row = 1, column = 0)
-singer1_label = Label(root, text = "Singer")
-singer1_label.grid(row = 2, column = 0)
+# my_frame = Frame(root)
+# my_scrollbar = Scrollbar(my_frame, orient = VERTICAL)
 
+# listbox = Listbox(my_frame, height = 20, width = 20, yscrollcommand = my_scrollbar.set)
+
+listbox = Listbox(root, height = 20, width = 20)
+listbox.grid(row = 1, column = 3, rowspan = 5)
+# # listbox.grid(row = 1, column = 3, rowspan = 5, selectmode=root.SINGLE)
+
+# my_scrollbar.config(command = listbox.yview)
+# my_scrollbar.pack(side=RIGHT, fill=Y)
+# my_frame.pack()
+
+
+var = StringVar()
+song_title = Label(root, font="Helvetica 12 bold", textvariable=var)
+# song_title.pack()
+song_title.grid(row = 3, column = 1)
 
 #class with all functions
-class player():
-    #intializing function - I think probelm is here - didn't intialize stuff - not sure how
-    def __init__(self, newWindow):
-        self.newWindow = newWindow
+class player(str):
 
-
-    #adding user input to database
-    def submit():
-        conn = sqlite3.connect('button.db')
+    def browseFiles():# browsefiles Function for opening the file explorer window
+        conn = sqlite3.connect('mp3db.db')
         c = conn.cursor()
-        c.execute("INSERT INTO buttontable VALUES(:song1, :singer1)", {'song1': song1.get(),'singer1': singer1.get()})
 
+        filename = filedialog.askopenfilename(initialdir = r"C:\Users\srini\TanviPython", title = "Select a File", filetypes =  (("MP3 File", "*.mp3"),))
+        c.execute("INSERT INTO mp3table VALUES(:playlist)", {'playlist': filename})
+
+        listbox.insert(END, filename)
         conn.commit()
         conn.close()
-        #clears textboxes
-        # song1.delete(0, END)
-        # singer1.delete(0, END)
+
 
     #query function - displays playlist in a listbox
     def query():
-        conn = sqlite3.connect('button.db')
+        conn = sqlite3.connect('mp3db.db')
         c = conn.cursor()
-        c.execute("SELECT *, oid  FROM buttontable")
+        c.execute("SELECT *, oid  FROM mp3table")
         records = c.fetchall()
         print_records = ''
         for record in records:
             print_records += str(record[0]) + " " + str(record[1]) + "\n"
-
-        print(print_records)#need to change this to Listbox - not sure how
-
-        # query_label = Label(root, text = print_records)
-        # query_label.grid(row = 7, column = 2)
-
-        # listbox = Listbox(root, height = 30, width = 30)
-        # listbox.grid(row = 7, column = 2)
-        # listbox.insert(END, print_records)
+            listbox.insert(END, record)
 
         conn.commit()
         conn.close()
 
-    #delete function on main window - opens a new window asking user to enter which song to be deleted
-    def delete1():
-        conn = sqlite3.connect('button.db')
+
+
+    def deleteSong():#deleteSong function deletes the selected song in the playlist
+        # Delete from Listbox
+        selection = listbox.curselection()
+        listbox.delete(selection[0])
+
+        conn = sqlite3.connect('mp3db.db')
         c = conn.cursor()
-        global newWindow
 
-        global song2 #problem maybe here too - not sure how to use variables from one function in another function
-        #think we need to intialize in __init__ function
-        #need to use this variable in delete2 fuction
-        newWindow = Toplevel(root)#new window - second window
-        newWindow.title("Deleting a song")
-        newWindow.geometry("300x300")
-        Label(newWindow, text ="Which song do you want to delete?").pack()
-        song2 = Entry(newWindow, width = 30)#takes user input
-        song2.pack()
-        song2_label = Label(newWindow, text ="Song: ").pack()
+        # row_id=int(selection[0])
+        # print(row_id)
+        # c.execute("DELETE FROM mp3table WHERE playlist = ?",(row_id+1,))
 
-        print(song2.get()) #doesnt work gives empty space
 
-        #write code for - inserting song 2 values in db
-        #make connection to db
-        #check if song1 == song2 and then delete if yes, move on if no
-
-        Button(newWindow, text ="Delete2", command = player.delete2).pack()#second delete button defined here - again - dont know how to link this to delete2 function
-
+        # c.execute("DELETE from mp3table WHERE playlist =?" , (playlist,))
+        # print(str(id))
         conn.commit()
         conn.close()
 
-    def delete2():
-        conn = sqlite3.connect('button.db')
+
+    def playSong():#playSong function plays the selected song in the playlist
+        # pygame.mixer.music.load(listbox.get(ACTIVE))
+        # var.set(listbox.get(ACTIVE))
+        # pygame.mixer.music.play()
+        return
+
+    def pauseSong():#pauseSong function pauses the selected song in the playlist
+        return
+
+
+    def deleteAll():
+        conn = sqlite3.connect('mp3db.db')
         c = conn.cursor()
-        # c.execute("DELETE from buttontable WHERE song == 'disco'")
-        print("Song deleted.")#wrote somthing for time being
+
+        c.execute("DELETE from mp3table")
 
         conn.commit()
         conn.close()
 
 
-    #buttons
-    submit_btn = Button(root, text = "Add song to database", command = submit)
-    submit_btn.grid(row = 3, column = 0, columnspan = 2, pady = 10, padx = 10, ipadx = 80)
+    #defining button properties
+    del_btn = Button(root, text ="Delete song", command = deleteSong)
+    del_btn.grid(row = 1, column = 2, pady = 5, padx = 5, ipadx = 50)
 
-    query_btn = Button(root, text = "Show records", command = query)
-    query_btn.grid(row = 4, column = 0, columnspan = 2, pady = 10, padx = 10, ipadx = 80)
+    add_btn = Button(root, text ="Add song", command = browseFiles)
+    add_btn.grid(row = 1, column = 1, pady = 5, padx = 5, ipadx = 50)
 
-    del1_btn = Button(root, text ="Delete1", command = delete1)
-    del1_btn.grid(row = 6, column = 0, columnspan=2, pady = 10, padx = 10, ipadx = 80)
+    play_btn = Button(root, text ="Play song", command = playSong)
+    play_btn.grid(row = 2, column = 1, pady = 5, padx = 5, ipadx = 50)
 
-    # listbox = Listbox(root, height = 10, width = 15, bg = "grey", activestyle = 'dotbox', font = "Helvetica", fg = "yellow")
+    pause_btn = Button(root, text ="Pause song", command = pauseSong)
+    pause_btn.grid(row = 2, column = 2, pady = 5, padx = 5, ipadx = 50)
+
+    deleteAll = Button(root, text ="Delete All", command = deleteAll)
+    deleteAll.grid(row = 3, column = 2, pady = 5, padx = 5, ipadx = 50)
+
+    show_records = Button(root, text ="Show Records", command = query)
+    show_records.grid(row = 4, column = 1, pady = 5, padx = 5, ipadx = 50)
+
 
 #object for the class
 p = player(root)
 #commiting changes to databse and closing connection
 conn.commit()
 conn.close()
-#think this should be p.mainloop() - for now this statement runs the tkinter window
 root.mainloop()
